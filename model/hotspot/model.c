c72471d47148d0aff0db872c9ad5c953057dd96d
Saurabh <saurabh@Saurabhs-MacBook-Pro.local>
2025-02-24 11:18:20 +0100

    updated model.c

--- model/hotspot/model.c
+++ model/hotspot/model.c
@@ -1,13 +1,8 @@
 /*
- * model file for HARM3D data
+ * model file for hotspot model
  *
- * Please note that most of the code for the harm3d model was adapted
- * from GRMONTY (Dolence et al., 2009).
- *
- * GRMONTY is released under the GNU GENERAL PUBLIC LICENSE.
- * Modifications were made in August 2016 by T. Bronzwaer and
- * J. Davelaar.
- * Modifications were made in August 2022 by J. Davelaar
+ * A modified version from ipole RIAF
+ * In progress : Saurabh February, 20225
  */
 
 #include "definitions.h"
